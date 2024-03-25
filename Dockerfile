32be6d29e1534ce34f6ef4e8bfbd646ed7df476c
zhe-hong-Lin <zhehome1021@gmail.com>
2024-03-26 01:09:29 +0800

    feat: 調整requirements.txt 以及Docker設定成功 build

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,23 @@
 # Use an official Python runtime as a parent image
 FROM python:3.9.12
 
-# Set environment variables
+# prevent Python from writing .pyc files
 ENV PYTHONDONTWRITEBYTECODE 1
+
+# ensure Python output is sent directly to the terminal without buffering
 ENV PYTHONUNBUFFERED 1
 
 # Set the working directory
 WORKDIR /app
 
 # Install dependencies
+# 將 pip install 進行更新
+RUN pip install --upgrade pip
 COPY requirements.txt /app/
 RUN pip install -r requirements.txt
+
+# Expose the port the app will run on
+EXPOSE 8000
 
 # Copy the project code into the container
 COPY . /app/
--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,19 +22,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
+import environ
 
-SECRET_KEY = config('SECRET_KEY')
+env = environ.Env()
+env.read_env()
 
 # Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('MYSQL_DATABASE'),
-        'USER': config('MYSQL_USER'),
-        'PASSWORD': config('MYSQL_PASSWORD'),
-        'HOST': config('DB_HOST', 'db'),  # Use 'db' as default from .env
-        'PORT': config('DB_PORT', '3306'),  # Use '3306' as default from .env
+        'NAME': env('MYSQL_DATABASE'),
+        'USER': env('MYSQL_USER'),
+        'PASSWORD': env('MYSQL_PASSWORD'),
+        'HOST': env('DB_HOST', default='db'),  # Use 'db' as default from .env
+        'PORT': env('DB_PORT', default='3306'),  # Use '3306' as default from .env
         'OPTIONS': {
             'sql_mode': 'traditional',
         }
@@ -42,13 +44,12 @@ DATABASES = {
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -56,6 +57,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+# Third Plugin App
+INSTALLED_APPS += [
+    'rest_framework',
 ]
 
 # CUSTOM APP
@@ -95,15 +101,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
@@ -150,3 +147,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom User Model
 AUTH_USER_MODEL = "users.CustomUser"
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # 新增這一行(Heroku)
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+#region (REST_FRAMEWORK)
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+#endregion
